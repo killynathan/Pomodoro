9da1f72ed42a31c049287b247dd15d3397927ab3
nathan <nathankou12@gmail.com>
2017-01-22 15:42:14 -0600

    fixed bugs added sound

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ var resumeButton = document.getElementById("resume");
 var typeDisplay = document.getElementById("type");
 var filler = document.getElementById("filler");
 
-var seconds = 1500; // how many seconds there is remaining
+var seconds = 5;//1500; // how many seconds there is remaining
 var isPaused = false;
 var setIntervalID;
 var started = false; // so user can't have multiple startCountdown()'s starting setIntervals and running down the time faster
+var inBreak = false;
+
+var ding = new Audio('ding.mp3');
+var dingDuration = 0;
 
 function secondsToTime(_seconds) {
 	var mins = Math.floor(_seconds / 60);
@@ -22,6 +26,7 @@ function secondsToTime(_seconds) {
 function startCountdown() {
 	if (!started) {
 		started = true;
+		inBreak = false;
 		setIntervalID = setInterval(decrementTime, 1000);
 		filler.className = "startAnimation";
 		filler.style.animationDuration = seconds + "s";
@@ -44,14 +49,19 @@ function resetCountdown() {
 	timeDisplay.innerHTML = secondsToTime(seconds);
 	filler.className = "";
 	started = false;
+	isPaused = false;
+	filler.style.animationPlayState = "running";
+	dingDuration = 0;
+	typeDisplay.innerHTML = "Session";
 }
 
 function startBreak() {
+	inBreak = true;
 	seconds = 300;
+	dingDuration = 0;
 	timeDisplay.innerHTML = secondsToTime(seconds);
 	typeDisplay.innerHTML = "Break";
 
-	filler.className = "";
 	filler.className = "startAnimation";
 	filler.style.animationDuration = seconds + "s";
 }
@@ -59,7 +69,19 @@ function startBreak() {
 function decrementTime() {
 	if (!isPaused) {
 		if (seconds == 0) {
-			startBreak();
+			filler.className = "";
+			if (inBreak) {
+				inBreak = false;
+				ding.play();
+				resetCountdown();
+			}
+
+			if (dingDuration < 5) {
+				if (dingDuration % 2 == 0) {ding.currentTime = 0; ding.play();} 
+				dingDuration++;
+				return; 
+			} 
+			else startBreak();
 		}
 		seconds--;
 		timeDisplay.innerHTML = secondsToTime(seconds);
